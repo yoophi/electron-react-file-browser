33ec5eabf7996d2a5956081f9dcbfd4c993c4013
Pyunghyuk Yoo <yoophi@gmail.com>
2021-02-07 21:54:59 +0900

    add files list sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import icon from '../assets/icon.svg';
 import './App.global.css';
 
+import React, { useCallback, useEffect, useState } from 'react';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+
+import fs from 'fs';
+import icon from '../assets/icon.svg';
+
 const Hello = () => {
+  const [files, setFiles] = useState([]);
+  const [error, setError] = useState(null);
+  const getFiles = useCallback(() => {
+    fs.readdir('/', (err, files) => {
+      if (err) {
+        setError(error);
+        return;
+      }
+
+      setFiles(files);
+    });
+  }, []);
+  useEffect(() => {
+    getFiles();
+  }, []);
+
   return (
     <div>
       <div className="Hello">
         <img width="200px" alt="icon" src={icon} />
       </div>
       <h1>electron-react-boilerplate</h1>
+      <pre>{JSON.stringify(files, null, 2)}</pre>
       <div className="Hello">
         <a
           href="https://electron-react-boilerplate.js.org/"
